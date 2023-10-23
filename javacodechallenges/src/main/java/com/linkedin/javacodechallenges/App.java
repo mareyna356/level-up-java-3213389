19ffa82270a7462c974a8ae8def786a854533010
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-10-23 03:02:18 +0000

    Challenge completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,13 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+public class App {
+    public static void main(String[] args) {
+        Person marco = new Person("Marco", "Reyna Vargas", 24);
+        Person mario = new Person("Mario", "Mario", 35);
+        Person luigi = new Person("Luigi", "Mario", 35);
+
+        marco.introduction();
+        mario.introduction();
+        luigi.introduction();
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,44 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void introduction() {
+    System.out
+        .println("Hello! My name is " + this.getFirstName()
+            + " " + this.getLastName() + ". I am "
+            + this.getAge() + " years old.");
+  }
 }
