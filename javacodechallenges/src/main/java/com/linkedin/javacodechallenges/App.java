de8cb24686d15efb964bcc63663b7c661c8636f6
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-10-23 00:11:45 +0000

    Challenge complete

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        return password.matches(".{6,}")
+                && password.matches(".*[0-9].*")
+                && password.matches(".*[a-z].*")
+                && password.matches(".*[A-Z].*");
     }
 
     public static void main(String[] args) {
