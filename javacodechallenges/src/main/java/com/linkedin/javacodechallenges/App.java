40c46b9d49507275b4fec5c6066d4e15c15ddf94
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-10-23 03:36:59 +0000

    Challenge completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,19 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    // Create function to calculate the date that's 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class App {
+    // Create function to calculate the date that's
     // 100 days from now
 
-    public static void main( String[] args )
-    {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+    public static LocalDate hundredDaysFromNow(LocalDate date) {
+        final int daysIntoFuture = 100;
+        return date.plusDays(daysIntoFuture);
+    }
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("100 days from now is... " + hundredDaysFromNow(date));
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,6 +1,8 @@
 package com.linkedin.javacodechallenges;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
 
 import org.junit.Test;
 
@@ -9,12 +11,18 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void hundredDaysFromNow() {
+        LocalDate d1 = LocalDate.of(2020, 1, 1);
+        assertEquals(LocalDate.of(2020, 4, 10),
+                App.hundredDaysFromNow(d1));
+
+        LocalDate d2 = LocalDate.of(2020, 11, 6);
+        assertEquals(LocalDate.of(2021, 2, 14),
+                App.hundredDaysFromNow(d2));
+
+        LocalDate d3 = LocalDate.of(2022, 11, 8);
+        assertEquals(LocalDate.of(2023, 2, 16),
+                App.hundredDaysFromNow(d3));
     }
 }
