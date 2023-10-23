54ee6e1dd180bc970cb6910d5251749ecdd16c8b
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-10-23 01:31:04 +0000

    challenge completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,46 @@
+package com.linkedin.javacodechallenges;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
 
+  private int points;
+  private static int startPoints = 10;
+  private String option;
+  private Scanner sc = new Scanner(System.in);
+  private Random random = new Random();
+  private int chance;
+
+  private void askInput() {
+    System.out.println("Current points: " + this.points + "\nTake a chance to double your points? Y or N");
+    this.option = this.sc.nextLine();
+  }
+
+  private void printLoss() {
+    System.out.println("You lost! Your points are now 0.");
+  }
+
+  private void printGoodbye() {
+    System.out.println("Thanks for playing. Goodbye!");
+  }
+
   public void playGame() {
-    // TODO: Implement method
+    this.points = startPoints;
+
+    askInput();
+    while (this.option.equalsIgnoreCase("y")) {
+      this.chance = this.random.nextInt(2);
+      if (this.chance == 1)
+        this.points *= 2;
+      else
+        this.points = 0;
+      if (this.points == 0) {
+        printLoss();
+        break;
+      }
+      askInput();
+    }
+    printGoodbye();
   }
 }
