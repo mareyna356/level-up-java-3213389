9a58ca31353aa6285f7354f646bf3270ec8888df
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-10-23 04:56:16 +0000

    Challenge completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,14 +1,34 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class App {
     public static List<String> findStudentsWithIncompleteVolunteerEvents(
             List<String> students,
             Map<String, List<String>> attendeesMapping) {
-        // TODO: implement function
-        return List.of();
+        List<String> studentsWithIncompleteVolunteerEvents = new ArrayList<String>();
+
+        for (String student : students) {
+            int numEvents = 0;
+            Set<Map.Entry<String, List<String>>> entrySetAttendeesMapping = attendeesMapping.entrySet();
+            for (Map.Entry<String, List<String>> entry : entrySetAttendeesMapping) {
+                List<String> attendees = entry.getValue();
+                if (attendees.contains(student))
+                    numEvents++;
+                if (numEvents == 2)
+                    break;
+            }
+            if (numEvents < 2)
+                studentsWithIncompleteVolunteerEvents.add(student);
+        }
+
+        String[] temp = studentsWithIncompleteVolunteerEvents
+                .toArray(new String[studentsWithIncompleteVolunteerEvents.size()]);
+
+        return List.of(temp);
     }
 
     public static void main(String[] args) {
