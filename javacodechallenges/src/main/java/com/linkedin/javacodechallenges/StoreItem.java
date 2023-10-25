69929160fa6a5b48b2de30e52cb64001f1100196
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-10-25 00:18:08 +0000

    Challenge completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
@@ -1,7 +1,9 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,12 +18,21 @@ public class StoreItem {
   double discount;
 
   public static Optional<StoreItem> findLeastExpensive(Collection<StoreItem> items) {
-    // TODO: Implement
-    return Optional.empty();
+    return items.stream().min(new StoreItemPriceComparator());
   }
 
   @Override
   public String toString() {
     return "Name: " + name + ", " + "Retail price: " + retailPrice + ", " + "Discount " + discount;
+  }
+}
+
+class StoreItemPriceComparator implements Comparator<StoreItem> {
+
+  @Override
+  public int compare(StoreItem item1, StoreItem item2) {
+    double item1Total = item1.getRetailPrice() * (1 - item1.getDiscount());
+    double item2Total = item2.getRetailPrice() * (1 - item2.getDiscount());
+    return Double.compare(item1Total, item2Total);
   }
 }
