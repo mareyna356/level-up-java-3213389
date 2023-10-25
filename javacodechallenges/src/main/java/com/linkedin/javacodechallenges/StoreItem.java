738a02c416743949852ee87432b044da01e2717d
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-10-25 01:57:16 +0000

    Solution made with alternative

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
@@ -1,6 +1,7 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Optional;
 
 import lombok.AllArgsConstructor;
@@ -15,9 +16,15 @@ public class StoreItem {
   double retailPrice;
   double discount;
 
+  private double discountedPrice() {
+    return this.getRetailPrice() * (1 - this.getDiscount());
+  }
+
   public static Optional<StoreItem> findLeastExpensive(Collection<StoreItem> items) {
-    // TODO: Implement
-    return Optional.empty();
+    return items.stream()
+        .min(Comparator.comparing(StoreItem::discountedPrice));
+    //return items.stream()
+    //    .min(Comparator.comparing((item) -> item.getRetailPrice() * (1 - item.getDiscount())));
   }
 
   @Override
