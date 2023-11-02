b320a5050b1e86ec28f84cd3faa2c94c98ea5df5
mareyna356 <116867368+mareyna356@users.noreply.github.com>
2023-11-02 05:56:18 +0000

    Challenge completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,7 +1,70 @@
 package com.linkedin.javacodechallenges;
 
-public class App {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static HashMap<String, Integer> getTicketHolders(String fileName) throws FileNotFoundException, IOException {
+        File file = new File(fileName);
+        HashMap<String, Integer> ticketHolders = new HashMap<String, Integer>();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = reader.readLine(); // header
+        if (line != null) { // if file is not empty
+            line = reader.readLine();
+            while (line != null) {
+                String[] ticketHolder = StringUtils.split(line, ',');
+                ticketHolders.put(ticketHolder[0], Integer.parseInt(ticketHolder[1]));
+                line = reader.readLine();
+            }
+        }
+        reader.close();
+        return ticketHolders;
+    }
+
+    public static void main(String[] args) {
+        try {
+            HashMap<String, Integer> ticketHolders = getTicketHolders("ticketholders.csv");
+            if (ticketHolders.isEmpty()) {
+                System.out.println("The CSV file contains no entries.");
+                return;
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ticket holder name:");
+            String ticketHolderName = scanner.nextLine();
+
+            if (ticketHolders.containsKey(ticketHolderName)) {
+                int numTickets = ticketHolders.get(ticketHolderName).intValue();
+
+                if (numTickets > 0) {
+                    System.out.println("Number of attendees:");
+                    String numAttendeesString = scanner.nextLine();
+
+                    int numAttendeesInt = Integer.parseInt(numAttendeesString);
+                    if (numAttendeesInt > 0) {
+                        boolean access = numTickets >= numAttendeesInt;
+                        System.out.println(ticketHolderName + " has " + numTickets + " ticket"
+                                + (numTickets == 1 ? "" : 's') + ", " + (access ? "" : "not ") + "enough for "
+                                + numAttendeesInt + " attendee"
+                                + (numAttendeesInt == 1 ? "" : 's')
+                                + ": access " + (access ? "granted." : "denied."));
+                    } else
+                        System.out.println("No attendees present. Operation cancelled.");
+                } else
+                    System.out.println("This ticket holder hasn't purchased any tickets: access denied.");
+            } else
+                System.out.println("That ticket holder doesn't exist.");
+
+            scanner.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,37 @@
 package com.linkedin.javacodechallenges;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    private final ByteArrayOutputStream printOut = new ByteArrayOutputStream();
+    private HashMap<String, Integer> example = new HashMap<String, Integer>(
+            Map.of("Jessica Jones", 2, "Rebecca Johnson", 3, "Sarah Roberts", 1, "Rafa Marquez", 0));
+
+    @Before
+    public void setUpEach() {
+        System.setOut(new PrintStream(printOut));
+    }
+
+    @After
+    public void cleanUpEach() {
+        System.setOut(System.out);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testGetTicketHolders() throws Exception {
+        HashMap<String, Integer> test = App.getTicketHolders("../ticketholders.csv");
+        assertEquals(example, test);
     }
 }
